5eda1a77c830e14c5e4ce4ad0c85f1a4fabcbb92
Vlad Chervanev <vlad.chervanev@castle.co>
2016-03-10 23:17:48 +0400

    [wip] input data converter

--- data/loader.rb
+++ data/loader.rb
@@ -22,18 +22,87 @@ end
 
 p CONST::INTERESTS
 
+input.each { |row| row[CONST::IMPORTANCE] = row[CONST::IMPORTANCE].to_i; row[CONST::INTERESTS] = row[CONST::INTERESTS].tr('{}"', '').split(',') }
 input.sort! { |x,y| y[CONST::IMPORTANCE] <=> x[CONST::IMPORTANCE]}
 
-class String
-  def fix(length=15)
-    self.ljust(length)[0..length-1]
+general_queue = input.map { |row| [row[CONST::ID], row[CONST::IMPORTANCE]]}
+
+total_interests = input.inject(0){ |a,b| a + b[CONST::INTERESTS].length }
+
+queues = Hash.new{|h,k| h[k] = [] }
+input.each do |row|
+  row[CONST::INTERESTS].each do |interest|
+    queues[interest.to_sym].push [row[CONST::ID], row[CONST::IMPORTANCE]]
   end
 end
 
-def to_row(array)
-  array.map(&:fix).join("|")
+total_converted_interests = queues.map {|k,v| v.length}.inject(0){|a,b|a+b}
+raise 'test failed' unless total_interests == total_converted_interests
+
+class QueueAdapter
+  attr_reader :eof, :iterator
+  def initialize(array)
+    @iterator = array.each
+    @eof = false
+  end
+
+  def next
+    eof ? nil : safe_next
+  end
+
+  def safe_next
+    iterator.next
+  rescue StopIteration
+    @eof = true
+    nil
+  end
 end
 
-puts to_row(HEADERS)
-input.each { |x| puts to_row(x)}
+# inline tests
+q = QueueAdapter.new([4])
+raise 'test failed' if q.eof
+raise 'test failed' unless q.next == 4
+raise 'test failed' if q.eof
+raise 'test failed' unless q.next.nil?
+raise 'test failed' unless q.eof
+
+
+class Mixer
+
+  def mix(queue1, queue2)
+    @listed = {}
+    q1 = QueueAdapter.new(queue1)
+    q2 = QueueAdapter.new(queue2)
+    [].tap do |result|
+      until q1.eof && q2.eof
+        if (v = extract(q1))
+          result.push(v)
+        end
+        if (v = extract(q2))
+          result.push(v)
+        end
+      end
+    end
+  end
+
+  private
+
+  def extract(q)
+    while true
+      value = q.next
+      if value && @listed.include?(value[0])
+        next
+      else
+        @listed[value[0]] = true if value
+        return value
+      end
+    end
+  end
+end
+
+m = Mixer.new
+result = m.mix([[1],[2],[3]], [[4],[5],[6]])
+raise 'test failed' unless result == [[1], [4], [2], [5], [3], [6]]
+
+raise 'Need more tests'
 
